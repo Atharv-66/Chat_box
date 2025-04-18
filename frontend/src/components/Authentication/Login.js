d93dc8db168c421a3440ad4c5d1aec6e3d36aad8
Atharv-66 <atharvpandey112@gmail.com>
2025-04-19 01:06:10 +0530

    Update Login.js

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -59,7 +59,7 @@ console.log(JSON.stringify(data));
     } catch (error) {
       toast({
         title: "Error Occured!",
-        description: error.response.data.message,
+        description: error.response?.data?.message || error.message,
         status: "error",
         duration: 5000,
         isClosable: true,
